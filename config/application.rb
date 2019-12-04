436a7e3cacc7b184bd6270664fd43954a2033f19
Tong Dang Tinh <tinhtongdang@gmail.com>
2019-12-04 08:39:58 +0700

    Use i18n

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Pr1907FoodAndDrink
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :vi
+    config.i18n.available_locales = [:vi, :en]
   end
 end
