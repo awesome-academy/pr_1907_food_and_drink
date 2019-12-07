3dc672feeead92b0dac1d8432cd40b6222ea21bd
Tong Dang Tinh <tinhtongdang@gmail.com>
2019-12-07 14:32:49 +0700

    Using Devise Gem

--- db/migrate/20191202121153_create_users.rb
+++ db/migrate/20191202121153_create_users.rb
@@ -2,16 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
       t.string :phone
       t.string :avatar
-      t.string :password_digest
-      t.string :remember_digest
-      t.datetime :activated
-      t.datetime :activated_at
-      t.string :reset_digest
-      t.datetime :reset_sent_at
-
       t.timestamps
     end
   end
