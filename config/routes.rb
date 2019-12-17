7ed7f1e83e667e7269887547a5c536328e6910ec
YOUR NAME <YOUR@EMAIL.com>
2019-12-17 20:13:38 +0700

    reservation-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see 
+  # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
 
   scope "(:locale)", locale: /en|vi/ do
     root "pages#home"
     get "/about", to: "pages#about"
+    get "/revervation", to: "pages#revervation"
   end
-  devise_for :users, path: '', path_names: { sign_in: 'login', 
-    sign_out: 'logout', edit: "profile", confirmation: "confirmation", }, 
-    controllers: { omniauth_callbacks: "omniauth_callbacks", 
+  devise_for :users, path: '', path_names: { sign_in: 'login',
+    sign_out: 'logout', edit: "profile", confirmation: "confirmation", },
+    controllers: { omniauth_callbacks: "omniauth_callbacks",
     registrations: "registrations"}
 end
